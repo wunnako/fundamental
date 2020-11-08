d3cae3b97e6dd7a1d9a52e8e07b2debd5bb33b54
Wunna Ko <wunnakoko@gmail.com>
2020-11-08 16:59:43 +0000

    Newer ema

--- ema.py
+++ ema.py
@@ -1,33 +1,39 @@
-#!/usr/bin/env python
-
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
-
-import json
 import pandas as pd
+import datetime as dt
+import pandas_datareader.data as web
 import sys
+import numpy as np
 
+def computeRSI (data, time_window):
+    diff = data.diff(1).dropna()        # diff in one field(one day)
 
-def technical_indicator(ticker, api_key, period, type = "ema"):
-    response = urlopen("https://financialmodelingprep.com/api/v3/technical_indicator/daily/" + ticker + "?period=" + period + "&type=" + type + "&apikey=" + api_key)
-    data = json.loads(response.read().decode("utf-8"))
-
-    if 'Error Message' in data:
-        raise ValueError(data['Error Message'])
-        
-    data_formatted = {}
-    for value in data:
-        date = value['date']
-        
-        del value['date']
-        data_formatted[date] = value
+    #this preservers dimensions off diff values
+    up_chg = 0 * diff
+    down_chg = 0 * diff
     
-    return pd.DataFrame(data_formatted)
+    # up change is equal to the positive difference, otherwise equal to zero
+    up_chg[diff > 0] = diff[ diff>0 ]
+    
+    # down change is equal to negative deifference, otherwise equal to zero
+    down_chg[diff < 0] = diff[ diff < 0 ]
+    
+    # check pandas documentation for ewm
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.ewm.html
+    # values are related to exponential decay
+    # we set com=time_window-1 so we get decay alpha=1/time_window
+    up_chg_avg   = up_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    down_chg_avg = down_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    
+    rs = abs(up_chg_avg/down_chg_avg)
+    rsi = 100 - 100/(1+rs)
+    return rsi
 
+def get_data (ticker, start, end):
+
+	df = web.DataReader(ticker, 'yahoo', start, end)
+	df.reset_index(inplace=True)
+	df.set_index("Date", inplace=True)
+	return df
 
 try:
     ticker = sys.argv[1].upper()
@@ -35,10 +41,19 @@ except:
     print('usage : '+__file__+' <ticker>')
     sys.exit(1)
 
-api_key = "4d1d8612c6f15e10c2a2327977f81d43"
+start = dt.datetime(2020, 1, 1)
+end = dt.datetime.now()
+#ticker = "MU"
+df = get_data (ticker, start, end)
 
-techindicator = technical_indicator(ticker, api_key, "12")
+df['12dayEMA'] = df['Adj Close'].ewm(span=12, adjust=False).mean()
 
-techindicator.loc['%'] = (techindicator.loc['close']-techindicator.loc['ema'])*100/techindicator.loc['close']
+df['26dayEMA'] = df['Adj Close'].ewm(span=26, adjust=False).mean()
 
-print(techindicator.T.head())
+df['UpDown'] = np.where((df['12dayEMA'] >= df['26dayEMA']), "Up", "Down")
+
+df['RSI'] = computeRSI(df['Adj Close'], 14)
+
+
+#view DataFrame 
+print(df.tail())
